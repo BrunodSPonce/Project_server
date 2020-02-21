a983507b472ec552a0cd746c1baf1b1e94421255
BrunoSPonce <60431550+BrunoSPonce@users.noreply.github.com>
2020-02-21 15:38:45 -0300

    Recomeçando projeto angular

---
+++ PrototipoServidorAlura/src/app/app-routing.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { TabelaComponent } from './tabela/tabela.component';
+
+
+const routes: Routes = [{ path: 'inicio', component: TabelaComponent }];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
